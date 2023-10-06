0b6cbb64faab1dece182fc4ce023c03b8015df94
Reem <reemelhusseiny.1999@gmail.com>
2023-10-06 23:53:04 +0300

    sidebar

--- app/src/main.jsx
+++ app/src/main.jsx
@@ -2,6 +2,9 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.bundle.js';
+import 'bootstrap-icons/font/bootstrap-icons.css';
 import { BrowserRouter as Router } from "react-router-dom";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
