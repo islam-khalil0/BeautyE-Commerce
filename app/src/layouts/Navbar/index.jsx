ab8c1f441ad057efb2e10b72ca04fb3a87f7f015
mostafamahmoud99 <mm0766633@gmail.com>
2023-10-07 17:02:32 +0300

    navbar

--- app/src/layouts/Navbar/index.jsx
+++ app/src/layouts/Navbar/index.jsx
@@ -1,9 +1,139 @@
-import React from 'react'
-import Styles from "./navbar.module.css"
+import React, { useState } from "react";
+import { Badge, Container, Nav, Navbar } from "react-bootstrap";
+import { Link, NavLink } from "react-router-dom";
+import Styles from './navbar.module.css'
+import { AiOutlineUser } from "react-icons/ai";
+import { BsBag } from "react-icons/bs";
+import { FaBars } from "react-icons/fa";
 
 const index = () => {
+  const [openNav, setOpenNav] = useState(false);
+
   return (
-    <div>navBar</div>
+<>
+<Container>
+        <div className={Styles.navbar_desktop}>
+          <div className="d-flex justify-content-between align-items-center py-3">
+            <div>
+              <Link to="/" className={Styles.logo}>
+                DIVI
+              </Link>
+            </div>
+
+            <input
+              type="text"
+              className={`${Styles.search_input} mx-3`}
+              placeholder="Search for product or brand"
+            />
+            <div className="d-flex align-items-center gap-4">
+              <Nav.Link as={Link} className="">
+                <AiOutlineUser />
+                <span className={Styles.user_icon}>Account</span>
+              </Nav.Link>
+              <Nav.Link>
+                <div className="position-relative">
+                  <BsBag />
+                  <span className={Styles.cart_icon}>Cart</span>
+                  <Badge className={`${Styles.badge} bg-black`}>0</Badge>
+                </div>
+              </Nav.Link>
+            </div>
+          </div>
+          <Navbar expand="lg" className={`${Styles.navbar} p-0 p-lg-2`}>
+            <Navbar.Collapse id="basic-navbar-nav">
+              <Nav className="d-flex w-100 justify-content-evenly">
+                <Nav.Link as={NavLink} to="/">
+                  Brands
+                </Nav.Link>
+                <Nav.Link as={NavLink} to="/sunscreen">
+                  Sunscreen
+                </Nav.Link>
+                <Nav.Link as={NavLink} to="#link">
+                  Browse by
+                </Nav.Link>
+                <Nav.Link as={NavLink} to="#link">
+                  Bestsellers
+                </Nav.Link>
+                <Nav.Link as={NavLink} to="#link">
+                  Skin care
+                </Nav.Link>
+                <Nav.Link as={NavLink} to="#link">
+                  Hair care
+                </Nav.Link>
+                <Nav.Link as={NavLink} to="#link">
+                  Makeup
+                </Nav.Link>
+                <Nav.Link as={NavLink} to="#link">
+                  Tools & devices
+                </Nav.Link>
+                <Nav.Link as={NavLink} to="#link">
+                  Bath & body
+                </Nav.Link>
+              </Nav>
+            </Navbar.Collapse>
+          </Navbar>
+        </div>
+
+        <div className={Styles.navbar_mobile}>
+          <div className="d-flex justify-content-between align-items-center py-3">
+            <div className="d-flex gap-2  align-items-center">
+              <h4 className={`${Styles.toggle} m-0`} onClick={() => setOpenNav(!openNav)}>
+                <FaBars />
+              </h4>
+
+              <Link to="/" className={Styles.logo}>
+                DIVI
+              </Link>
+            </div>
+            <div className="d-flex align-items-center gap-4">
+              <Link>
+              <AiOutlineUser className="text-dark"/>
+                <span className={Styles.user_icon}>User</span>
+              </Link>
+              <Link>
+                <div className="position-relative">
+                <BsBag className="text-dark"/>
+                  <span className={Styles.cart_icon}>Cart</span>
+                  <Badge className={`${Styles.badge} bg-black`}>0</Badge>
+                </div>
+              </Link>
+            </div>
+          </div>
+          {openNav && (
+            <div className={Styles.navs}>
+              <Nav.Link as={NavLink} className="py-1" to="/">
+                Brands
+              </Nav.Link>
+              <Nav.Link as={NavLink} className="py-1" to="/sunscreen">
+                Sunscreen
+              </Nav.Link>
+              <Nav.Link as={NavLink} className="py-1" to="#link">
+                Browse by
+              </Nav.Link>
+              <Nav.Link as={NavLink} className="py-1" to="#link">
+                Bestsellers
+              </Nav.Link>
+              <Nav.Link as={NavLink} className="py-1" to="#link">
+                Skin care
+              </Nav.Link>
+              <Nav.Link as={NavLink} className="py-1" to="#link">
+                Hair care
+              </Nav.Link>
+              <Nav.Link as={NavLink} className="py-1" to="#link">
+                Makeup
+              </Nav.Link>
+              <Nav.Link as={NavLink} className="py-1" to="#link">
+                Tools & devices
+              </Nav.Link>
+              <Nav.Link as={NavLink} className="py-1" to="#link">
+                Bath & body
+              </Nav.Link>
+            </div>
+          )}
+          <input type="text" placeholder="Search for product or brand" className={Styles.search_input} />
+        </div>
+      </Container>
+</>
   )
 }
 
--- app/src/pages/Home/index.jsx
+++ app/src/pages/Home/index.jsx
@@ -1,10 +1,170 @@
-import React from 'react'
-import Styles from "./home.module.css"
+import React, { useState } from "react";
+import Styles from "./Home.module.css";
+import { Button, Card, Col, Container, Row } from "react-bootstrap";
+import { Navigation, Pagination } from "swiper/modules";
+
+import { Swiper, SwiperSlide } from "swiper/react";
+
+// Import Swiper styles
+import "swiper/css";
+import "swiper/css/navigation";
+import "swiper/css/pagination";
+import { MdFavoriteBorder } from "react-icons/md";
+import { AiFillStar } from "react-icons/ai";
+import { MdAddShoppingCart } from "react-icons/md";
+
+const sliderHome = [1, 2, 3, 4, 5, 6];
+
+const data = [
+  {
+    id: 1,
+    img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTSNBYXudKZdS1qhvlANYZ6bde369xX2QAEMg&usqp=CAU",
+    desc: "lorem",
+    price: 33,
+  },
+  {
+    id: 2,
+    img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTSNBYXudKZdS1qhvlANYZ6bde369xX2QAEMg&usqp=CAU",
+    desc: "lorem",
+    price: 33,
+  },
+  {
+    id: 3,
+    img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTSNBYXudKZdS1qhvlANYZ6bde369xX2QAEMg&usqp=CAU",
+    desc: "lorem",
+    price: 33,
+  },
+  {
+    id: 4,
+    img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTSNBYXudKZdS1qhvlANYZ6bde369xX2QAEMg&usqp=CAU",
+    desc: "lorem",
+    price: 33,
+  },
+  {
+    id: 5,
+    img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTSNBYXudKZdS1qhvlANYZ6bde369xX2QAEMg&usqp=CAU",
+    desc: "lorem",
+    price: 33,
+  },
+  {
+    id: 6,
+    img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTSNBYXudKZdS1qhvlANYZ6bde369xX2QAEMg&usqp=CAU",
+    desc: "lorem",
+    price: 33,
+  },
+  {
+    id: 7,
+    img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTSNBYXudKZdS1qhvlANYZ6bde369xX2QAEMg&usqp=CAU",
+    desc: "lorem",
+    price: 33,
+  },
+];
 
 const index = () => {
   return (
-    <div>home page</div>
-  )
-}
+    <>
+      <Swiper
+        // install Swiper modules
 
-export default index
+        modules={[Navigation, Pagination]}
+        className="my-5"
+        spaceBetween={40}
+        slidesPerView={1}
+        navigation
+        pagination={{ clickable: true }}
+        onSwiper={(swiper) => console.log(swiper)}
+        onSlideChange={() => console.log("slide change")}
+      >
+        {sliderHome.map((_item) => (
+          <SwiperSlide>
+            <Row className={Styles.home}>
+              <Col md={6} className={Styles.img_content}>
+                <img
+                  className="d-block w-100"
+                  src="https://cdn.logojoy.com/wp-content/uploads/20191023114758/AdobeStock_224061283-min.jpeg"
+                  alt=""
+                />
+              </Col>
+              <Col md={6} className="d-flex align-items-center">
+                <div className="p-3">
+                  <h2>
+                    New from Eminence <br className="d-none d-md-block" />{" "}
+                    Organic Skin Care
+                  </h2>
+                  <p className="py-2">
+                    Mineral protection that guards against sun{" "}
+                    <br className="d-none d-md-block" /> damage & leaves skin
+                    looking radiant.
+                  </p>
+                  <Button className="px-4">Shop now</Button>
+                </div>
+              </Col>
+            </Row>
+          </SwiperSlide>
+        ))}
+      </Swiper>
+
+      {/* brands card */}
+      <Container className="my-4">
+        <Swiper
+          // install Swiper modules
+          modules={[Navigation, Pagination]}
+          className=""
+          spaceBetween={40}
+          slidesPerView={2}
+          breakpoints={{
+            576: {
+              slidesPerView: 3,
+              spaceBetween: 20,
+            },
+            768: {
+              slidesPerView: 4,
+              spaceBetween: 20,
+            },
+            1024: {
+              slidesPerView: 5,
+              spaceBetween: 20,
+            },
+          }}
+          navigation
+          pagination={{ clickable: true }}
+          onSwiper={(swiper) => console.log(swiper)}
+          onSlideChange={() => console.log("slide change")}
+        >
+          {data.map((item) => (
+            <SwiperSlide>
+              <Card>
+                <img src={item.img} alt="" />
+                <div className="body-card p-2">
+                  <div className="d-flex justify-content-between align-content-center ">
+                    <div>
+                      <p className="fw-bold m-0">Brand Name</p>
+                      <p className="m-0">Brand information</p>
+                    </div>
+                    <div>
+                      <MdFavoriteBorder />
+                    </div>
+                  </div>
+                  <div className="rating">
+                    <AiFillStar className="text-warning" />
+                    <AiFillStar className="text-warning" />
+                    <AiFillStar className="text-warning" />
+                    <AiFillStar className="text-warning" />
+                    <AiFillStar className="text-warning" />
+                    <span className="rating-num">(5085)</span>
+                  </div>
+                  <p className="py-2 m-0">EGP 55.00</p>
+                  <Button className="w-100">
+                    <MdAddShoppingCart /> Add to cart
+                  </Button>
+                </div>
+              </Card>
+            </SwiperSlide>
+          ))}
+        </Swiper>
+      </Container>
+    </>
+  );
+};
+
+export default index;
