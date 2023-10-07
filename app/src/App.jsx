2d76075ad61e43991fe3e3edaf33527cbd326299
islam <eslamkhalil851@gmail.com>
2023-10-07 19:53:46 +0300

    Edit components

--- app/src/App.jsx
+++ app/src/App.jsx
@@ -1,12 +1,11 @@
 import "./App.css";
 import Routers from "./Routers/Routers";
-import Checkbox1 from "./components/ProductCard/Checkbox1";
+import Checkbox1 from "./components/CheckBoxOne/Checkbox1";
 
 function App() {
   return (
     <div>
-      {/* <Routers /> */}
-      <Checkbox1/> 
+      <Routers />
     </div>
   );
 }
