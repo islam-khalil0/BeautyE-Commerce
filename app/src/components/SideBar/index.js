fda29d75c142bfe954f1df0d732745ae35788b39
islam <eslamkhalil851@gmail.com>
2023-10-26 17:41:22 +0300

    new update of structur

---
+++ app/src/components/SideBar/index.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import Styles from "./sideBar.module.css"
+
+const index = () => {
+  return (
+    <div>index</div>
+  )
+}
+
+export default index
---
+++ app/src/pages/Verification/index.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import Styles from "./Verification.module.css"
+
+const index = () => {
+  return (
+    <div>index</div>
+  )
+}
+
+export default index
